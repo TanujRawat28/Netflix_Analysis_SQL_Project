a8dfedeec48c994568028ec52ae3563b8c8d7799
Tanuj Rawat <rawatanuj192@gmail.com>
2026-08-10 23:34:41 +0530

    Add files via upload

---
+++ Netflix_Business_Question.sql
@@ -0,0 +1,24 @@
+-- Business Question (22 Total)
+
+Q1: Count the number of Movies vs TV Shows.
+Q2: Find the most common rating for Movies and for TV Shows.
+Q3: List all movies released in a specific year (e.g. 2021).
+Q4: Find the top 5 countries with the most content on Netflix.
+Q5: Identify the longest movie (by duration in minutes).
+Q6: Number of content items added to Netflix each year.
+Q7: Number of content items in each genre.
+Q8: Top 10 directors with the most content.
+Q9: Content where country information is missing.
+Q10: content where director information is missing.
+Q11: Top 10 actors who've appeared in the most movies produced in India.
+Q12: Actor-Director collaborations.
+Q13: Content 'drought' years per country.
+Q14: Solo vs Ensemble content (avg casts size by type).
+Q15: Release-To-Netflix lag.
+Q16: Rating trend over time (% mature content per release year).
+Q17: Director specialization (specialization vs generalist).
+Q18: Monthly seasonality of content drops.
+Q19: Country content diversity index.
+Q20: "One-Hit" vs recurring directors.
+Q21: Casts overlap between countries (crossover talent).
+Q22: Each Country's most recent and oldest content addition
